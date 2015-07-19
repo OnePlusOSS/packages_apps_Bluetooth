ac1c91e9a4e378e96d9d46e7b2c77c2d1215755a
Nitin Shivpure <nshivpur@codeaurora.org>
2015-07-19 15:50:15 +0530

    MAP: MSE MAS Email Instance Support
    - Add support MAP Email Instance over L2CAP.
    
    CRs-fixed: 2040198
    Change-Id: I9c9ded56db491f80a7bc6d7dec68e6f0551612a2

--- Android.mk
+++ Android.mk
@@ -15,7 +15,8 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
-        $(call all-proto-files-under, src)
+        $(call all-proto-files-under, src) \
+        $(call all-java-files-under, ../../../vendor/qcom/opensource/bluetooth_ext/packages_apps_bluetooth_ext)
 
 LOCAL_PACKAGE_NAME := Bluetooth
 LOCAL_CERTIFICATE := platform
@@ -23,6 +24,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_JNI_SHARED_LIBRARIES := libbluetooth_jni
 LOCAL_JAVA_LIBRARIES := javax.obex telephony-common libprotobuf-java-micro services.net
 LOCAL_STATIC_JAVA_LIBRARIES := com.android.vcard bluetooth.mapsapi sap-api-java-static android-support-v4 services.net
+LOCAL_STATIC_JAVA_LIBRARIES += com.android.emailcommon
 LOCAL_PROTOC_OPTIMIZE_TYPE := micro
 
 LOCAL_REQUIRED_MODULES := bluetooth.default
