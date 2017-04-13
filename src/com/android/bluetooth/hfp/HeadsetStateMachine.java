0542ef29cc522107e5bdd484bd196d86c049dbd8
Satish Kodishala <skodisha@codeaurora.org>
2017-04-13 20:54:45 +0530

    Playback silence on SCO Rx during VR
    If VR app is opening only recording session, SCO Tx
    audio device only gets selected. For SCO to work
    properly, both SCO Rx and SCO Tx audio devices need
    to be selected. Start playback session for playing
    silence so that SCO Rx audio device will get selected.
    
    CRs-Fixed: 2032857, 2075749
    Change-Id: I45f1127b60c6a2ceec5e92c1af71bd5174c301eb
    
    Check for valid min buffer size of Audio track.
    
    Check for the minimum buffer size of Audio track. In failure cases
    the getMinBufferSize API would return negative value, resulting
    in BT Crash.
    
    CRs-Fixed: 2075811
    Change-Id: Iee76d27733ea9a85e634da2e0c2f1f8da36a3d0a
    
    Wait for AudioTrack thread to exit
    
    During SCO disconnection, wait for AudioTrack
    thread to exit. Also, cleanup AudioTrack in its
    thread rather than State Machine's thread.
    
    CRs-Fixed: 2088341
    Change-Id: I222214a216255124eecfdf4a43eb8b3c7a0a859d

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -77,6 +77,15 @@ import android.os.SystemProperties;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import android.telecom.TelecomManager;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Process;
+
+import java.lang.InterruptedException;
+import java.lang.Math;
+import java.lang.Runnable;
+
 final class HeadsetStateMachine extends StateMachine {
     private static final String TAG = "HeadsetStateMachine";
     private static final boolean DBG = Log.isLoggable("Handsfree", Log.VERBOSE);
@@ -214,6 +223,9 @@ final class HeadsetStateMachine extends StateMachine {
 
     private boolean mIsBlacklistedDevice = false;
 
+    private AudioPlayer mAudioPlayer = null;
+
+    private boolean mPlaybackSilence = false;
     // mCurrentDevice is the device connected before the state changes
     // mTargetDevice is the device to be connected
     // mIncomingDevice is the device connecting to us, valid only in Pending state
@@ -311,6 +323,12 @@ final class HeadsetStateMachine extends StateMachine {
         addState(mMultiHFPending);
 
         setInitialState(mDisconnected);
+
+        mPlaybackSilence = SystemProperties.getBoolean("persist.bt.hfp.playbacksilence", true);
+        Log.d(TAG, "mPlaybackSilence is " + mPlaybackSilence);
+
+        if (mPlaybackSilence)
+            mAudioPlayer = new AudioPlayer();
     }
 
     static HeadsetStateMachine make(HeadsetService context) {
@@ -342,6 +360,14 @@ final class HeadsetStateMachine extends StateMachine {
             broadcastConnectionState(mCurrentDevice, BluetoothProfile.STATE_DISCONNECTED,
                                      BluetoothProfile.STATE_CONNECTED);
         }
+
+        if (mPlaybackSilence &&
+            (mAudioPlayer != null) &&
+            mAudioPlayer.isPlaying()) {
+            Log.d(TAG, "SCO disconnected, stop audio playback");
+            mAudioPlayer.stop();
+        }
+
         quitNow();
         Log.d(TAG, "Exit doQuit()");
     }
@@ -1296,6 +1322,15 @@ final class HeadsetStateMachine extends StateMachine {
                     mAudioState = BluetoothHeadset.STATE_AUDIO_CONNECTED;
                     setAudioParameters(device); /*Set proper Audio Paramters.*/
 
+                    // Start playing silence if app is not opening playback session
+                    // and selecting device for SCO Rx
+                    if (mPlaybackSilence &&
+                        (mAudioPlayer != null) &&
+                        !mAudioPlayer.isPlaying()) {
+                        Log.d(TAG, "SCO connected, starting audio playback");
+                        mAudioPlayer.start();
+                    }
+
                     mAudioManager.setParameters("BT_SCO=on");
                     mAudioManager.setBluetoothScoOn(true);
                     mActiveScoDevice = device;
@@ -1835,6 +1870,13 @@ final class HeadsetStateMachine extends StateMachine {
                         } else {
                             log("Sco disconnected for CS call, do not check network type");
                         }
+
+                        if (mPlaybackSilence &&
+                            (mAudioPlayer != null) &&
+                            mAudioPlayer.isPlaying()) {
+                            Log.d(TAG, "SCO disconnected, stop audio playback");
+                            mAudioPlayer.stop();
+                        }
                     }
                     mActiveScoDevice = null;
                     transitionTo(mConnected);
@@ -2364,6 +2406,16 @@ final class HeadsetStateMachine extends StateMachine {
                     }
                     mAudioState = BluetoothHeadset.STATE_AUDIO_CONNECTED;
                     setAudioParameters(device); /* Set proper Audio Parameters. */
+
+                    // Start playing silence if app is not opening playback session
+                    // and selecting device for SCO Rx
+                    if (mPlaybackSilence &&
+                        (mAudioPlayer != null) &&
+                        !mAudioPlayer.isPlaying()) {
+                        Log.d(TAG, "SCO connected, starting audio playback");
+                        mAudioPlayer.start();
+                    }
+
                     mAudioManager.setParameters("BT_SCO=on");
                     mAudioManager.setBluetoothScoOn(true);
                     mActiveScoDevice = device;
@@ -4303,6 +4355,126 @@ final class HeadsetStateMachine extends StateMachine {
         }
         Log.d(TAG, "Exit handleAccessPermissionResult()");
     }
+
+
+    public class AudioPlayer implements Runnable {
+        AudioTrack mAudioTrack;
+        int mBufferSize;
+
+        boolean mPlay;
+        boolean mIsPlaying;
+
+        short[] mAudioData;
+
+        Thread mFillerThread = null;
+
+        public AudioPlayer() {
+            mBufferSize = 0;
+            mBufferSize =
+                    AudioTrack.getMinBufferSize(
+                        8000,
+                        AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT);
+            mPlay = false;
+            mIsPlaying = false;
+            if (mBufferSize <= 0) {
+                Log.e(TAG, "audio track buffer size not valid");
+                return;
+            }
+
+            // setup audio data (silence will suffice)
+            mAudioData = new short[mBufferSize];
+            for (int index = 0; index < mBufferSize; index++) {
+                 mAudioData[index] = 0;
+            }
+        }
+
+        public boolean isPlaying() {
+            synchronized (this) {
+                return mIsPlaying;
+            }
+        }
+
+        public void start() {
+            if (mPlay == true)
+                return;
+
+            if (mBufferSize <= 0) {
+                mBufferSize =
+                        AudioTrack.getMinBufferSize(
+                            8000,
+                            AudioFormat.CHANNEL_OUT_MONO,
+                            AudioFormat.ENCODING_PCM_16BIT);
+
+                if (mBufferSize <= 0) {
+                    Log.e(TAG, "In start, audio track buffer size not valid");
+                    return;
+                }
+                // setup audio data (silence will suffice)
+                mAudioData = new short[mBufferSize];
+                for (int index = 0; index < mBufferSize; index++) {
+                     mAudioData[index] = 0;
+                }
+            }
+            mPlay = true;
+            mFillerThread = new Thread(this);
+            if(mFillerThread != null)
+               mFillerThread.start();
+        }
+
+        public void stop() {
+            mPlay = false;
+
+            try {
+                Log.d(TAG, "waiting for audio track thread to exit");
+                if(mFillerThread != null)
+                   mFillerThread.join();
+                Log.d(TAG, "audio track thread exited or timed out");
+            } catch (InterruptedException ex) {
+            }
+
+            mFillerThread = null;
+        }
+
+        @Override
+        public void run() {
+            mAudioTrack =
+                new AudioTrack(
+                    AudioManager.STREAM_VOICE_CALL,
+                    8000,
+                    AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    mBufferSize,
+                    AudioTrack.MODE_STREAM);
+
+            Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
+
+            if (mAudioTrack != null)
+                mAudioTrack.play();
+            synchronized (this) {
+                mIsPlaying = true;
+            }
+            while (mAudioTrack != null && mPlay) {
+                mAudioTrack.write(mAudioData, 0, mBufferSize);
+            }
+
+            if (mAudioTrack != null) {
+                Log.d(TAG, "stopping audio track");
+                mAudioTrack.stop();
+            }
+
+            synchronized (this) {
+                mIsPlaying = false;
+            }
+
+            Log.d(TAG, "releasing audio track");
+            if (mAudioTrack != null) {
+                mAudioTrack.release();
+                mAudioTrack = null;
+            }
+        }
+    }
+
 
     private static final String SCHEME_TEL = "tel";
 
